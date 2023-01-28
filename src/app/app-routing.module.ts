1458509c2958147b02bad6f8d2afe500b90b9535
GuilhermeVFSousa <guilhermevfsousa@gmail.com>
2023-01-28 14:06:35 -0300

    Configurando as rotas protegidas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,29 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Injector, NgModule } from '@angular/core';
+import { RouterModule, Routes, Router } from '@angular/router';
 import { ProdutoListComponent } from './components/produto-list/produto-list.component';
 import { ProdutoDetailsComponent } from './components/produto-details/produto-details.component';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
-import { OktaCallbackComponent } from '@okta/okta-angular';
+import { OktaAuthGuard, OktaCallbackComponent } from '@okta/okta-angular';
 import { LoginComponent } from './components/login/login.component';
+import { MembersPageComponent } from './components/members-page/members-page.component';
+import { OktaAuth } from '@okta/okta-auth-js';
+
+function sendToLoginPage(oktaAuth: OktaAuth, injector: Injector) {
+  //Usando o injector para acessar qualquer serviço disponibilizado dentro da nossa aplicação
+  const router = injector.get(Router);
+
+  // Redirecionando o user para nossa rota página de login personalizada
+  router.navigate(['/login']);
+}
 
 const routes: Routes = [
+  //protect route
+  {path: 'membros', component: MembersPageComponent, canActivate: [OktaAuthGuard], data: {onAuthRequired: sendToLoginPage}},
+
   {path: 'login/callback', component: OktaCallbackComponent},
   {path: 'login', component: LoginComponent},
+
   {path: 'checkout', component: CheckoutComponent},
   {path: 'cart-details', component: CartDetailsComponent},
   {path: 'produtos/:id', component: ProdutoDetailsComponent},
