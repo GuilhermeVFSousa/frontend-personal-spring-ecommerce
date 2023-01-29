48493d8841f0acbdbaff908cffa5d6b99fdc31bd
GuilhermeVFSousa <guilhermevfsousa@gmail.com>
2023-01-29 14:42:57 -0300

    Armazenando o e-mail do customer em localStorage e criada a classe OrderHistory

--- src/app/components/login-status/login-status.component.ts
+++ src/app/components/login-status/login-status.component.ts
@@ -12,6 +12,8 @@ export class LoginStatusComponent implements OnInit {
   isAuthenticated: boolean = false;
   userFullName: string = '';
 
+  storage: Storage = localStorage;
+
   constructor(
     private oktaAuthService: OktaAuthStateService,
     @Inject(OKTA_AUTH) private oktaAuth: OktaAuth
@@ -35,6 +37,13 @@ export class LoginStatusComponent implements OnInit {
     this.oktaAuth.getUser().then(
       (res) => {
         this.userFullName = res.name as string;
+
+        // receber o email do usuario autenticado no response
+        const theEmail = res.email;
+
+        // armazenar o e-mail em localStorage
+        this.storage.setItem('userEmail', JSON.stringify(theEmail));
+
       }
     )
   }
