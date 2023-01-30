3286accc26d80d8c1022cc2806920a0eba1c5087
GuilhermeVFSousa <guilhermevfsousa@gmail.com>
2023-01-30 16:31:31 -0300

    Ajustando o App Module para referenciar o Interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProdutoListComponent } from './components/produto-list/produto-list.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ProdutoService } from './services/produto.service';
 
 import ptBr from '@angular/common/locales/pt';
@@ -33,6 +33,7 @@ import { OktaAuth } from '@okta/okta-auth-js';
 import myAppConfig from 'src/app/config/my-app-config';
 import { MembersPageComponent } from './components/members-page/members-page.component';
 import { OrderHistoryComponent } from './components/order-history/order-history.component';
+import { AuthInterceptorService } from './services/auth-interceptor.service';
 
 const oktaConfig = myAppConfig.oidc;
 
@@ -72,7 +73,8 @@ registerLocaleData(ptBr);
   providers: [
     ProdutoService,
     { provide: LOCALE_ID, useValue: 'pt'},
-    { provide: OKTA_CONFIG, useValue: { oktaAuth } }
+    { provide: OKTA_CONFIG, useValue: { oktaAuth } },
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }
   ],
   bootstrap: [AppComponent]
 })
