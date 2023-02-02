a20c11d74ade082eed39304f09ff1ee30ba5245b
GuilhermeVFSousa <guilhermevfsousa@gmail.com>
2023-02-01 22:51:53 -0300

    CheckoutService - implementado PaymentIntent e add a biblioteca JS do Stripe no index.html

--- src/app/services/checkout.service.ts
+++ src/app/services/checkout.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Purchase } from '../models/purchase';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { PaymentInfo } from '../models/payment-info';
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +12,15 @@ export class CheckoutService {
 
   private purchaseUrl = `${environment.baseUrl}/api/checkout/purchase`;
 
+  private paymentIntentUrl = `${environment.baseUrl}/api/checkout/payment-intent`;
+
   constructor(private httpClient: HttpClient) { }
 
   placeOrder(purchase: Purchase): Observable<any> {
     return this.httpClient.post<Purchase>(this.purchaseUrl, purchase);
+  }
+
+  createPaymentIntent(paymentInfo: PaymentInfo): Observable<any> {
+    return this.httpClient.post<PaymentInfo>(this.paymentIntentUrl, paymentInfo);
   }
 }
